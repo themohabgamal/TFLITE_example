d9db19976c6aae25a4ed2aeb37cf68d9e2938b09
Mohab Gamal <104150116+themohabgamal@users.noreply.github.com>
2024-05-05 21:26:55 +0300

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,118 @@
+// import 'dart:convert';
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:tflite_v2/tflite_v2.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: ObjectDetectionScreen(),
+    );
+  }
+}
+
+class ObjectDetectionScreen extends StatefulWidget {
+  const ObjectDetectionScreen({super.key});
+
+  @override
+  _ObjectDetectionScreenState createState() => _ObjectDetectionScreenState();
+}
+
+class _ObjectDetectionScreenState extends State<ObjectDetectionScreen> {
+  final ImagePicker _picker = ImagePicker();
+  XFile? _image;
+  File? file;
+  var _recognitions;
+  var v = "";
+  // var dataList = [];
+  @override
+  void initState() {
+    super.initState();
+    loadmodel().then((value) {
+      setState(() {});
+    });
+  }
+
+  loadmodel() async {
+    await Tflite.loadModel(
+      model: "assets/model_unquant.tflite",
+      labels: "assets/labels.txt",
+    );
+  }
+
+  Future<void> _pickImage() async {
+    try {
+      final XFile? image = await _picker.pickImage(source: ImageSource.gallery);
+      setState(() {
+        _image = image;
+        file = File(image!.path);
+      });
+      detectimage(file!);
+    } catch (e) {
+      print('Error picking image: $e');
+    }
+  }
+
+  Future detectimage(File image) async {
+    var recognitions = await Tflite.runModelOnImage(
+      path: image.path,
+      numResults: 6,
+      threshold: 0.05,
+      imageMean: 127.5,
+      imageStd: 127.5,
+    );
+    setState(() {
+      _recognitions = recognitions;
+      v = recognitions.toString();
+      // dataList = List<Map<String, dynamic>>.from(jsonDecode(v));
+    });
+    print(_recognitions);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        toolbarHeight: 100,
+        title: const Text(
+          'Object Detection via TFLITE',
+          style: TextStyle(color: Colors.white),
+        ),
+        backgroundColor: Colors.deepPurple,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            if (_image != null)
+              Image.file(
+                File(_image!.path),
+                height: 200,
+                width: 200,
+                fit: BoxFit.cover,
+              )
+            else
+              const Text('Pick an image to identify'),
+            const SizedBox(height: 50),
+            ElevatedButton(
+              onPressed: _pickImage,
+              child: const Text('Pick Image from Gallery'),
+            ),
+            const SizedBox(height: 20),
+            Text(v),
+          ],
+        ),
+      ),
+    );
+  }
+}
